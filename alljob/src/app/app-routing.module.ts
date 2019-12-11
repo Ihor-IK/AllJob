5b262d1806d7e8cd9f8fe4a4f12fd3b343ccf44c
Ihor Kukharev <kuharev@ukr.net>
2019-12-10 23:23:31 -0500

    Main layout created

--- alljob/src/app/app-routing.module.ts
+++ alljob/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { JobPageComponent } from './job-page/job-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //root object will represent layout
+  {
+    path: '', component: MainLayoutComponent, children:[
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'job/:id', component: JobPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
