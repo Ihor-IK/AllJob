19e9ac2cde9f86ef68af2b01c9c7afa96fd5c8fb
Ihor Kukharev <kuharev@ukr.net>
2019-12-15 13:48:37 -0500

    Added token storage

---
+++ alljob/src/environments/interface.ts
@@ -0,0 +1,12 @@
+export interface Environment {
+    apiKey: string,
+    production: boolean
+        // apiKey: "AIzaSyBMtwtbbRtUG7Z-KHxZhA8eXu5NrXEF-Yw",
+        // authDomain: "alljob-56745.firebaseapp.com",
+        // databaseURL: "https://alljob-56745.firebaseio.com",
+        // projectId: "alljob-56745",
+        // storageBucket: "alljob-56745.appspot.com",
+        // messagingSenderId: "315079769013",
+        // appId: "1:315079769013:web:d066b4a9729077b30337fc"
+      
+}
