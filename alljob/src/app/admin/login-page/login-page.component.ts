e0598dcc5beae69eda88d8aa048fd5d8f7328e57
Ihor Kukharev <kuharev@ukr.net>
2019-12-15 23:37:18 -0500

    Added error processing log in

--- alljob/src/app/admin/login-page/login-page.component.ts
+++ alljob/src/app/admin/login-page/login-page.component.ts
@@ -17,7 +17,7 @@ export class LoginPageComponent implements OnInit {
   submitted = false;
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router
   ) {
   }
@@ -50,7 +50,8 @@ export class LoginPageComponent implements OnInit {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
       this.submitted = false
+    }, () => {
+      this.submitted = false
     })
-
   }
 }
