a63fe13264290627746ce90044e60165605df029
Ihor Kukharev <kuharev@ukr.net>
2019-12-16 23:51:13 -0500

    Added guard (route protection)

--- alljob/src/app/admin/login-page/login-page.component.ts
+++ alljob/src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import { Router } from '@angular/router';
-
 import {User} from '../../shared/interfaces';
-import { AuthService } from '../shared/services/auth.service';
-
+import {AuthService} from '../shared/services/auth.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -14,15 +12,23 @@ import { AuthService } from '../shared/services/auth.service';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup
-  submitted = false;
+  submitted = false
+  message: string
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
   }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'Please enter data'
+      }
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
@@ -41,11 +47,12 @@ export class LoginPageComponent implements OnInit {
     }
 
     this.submitted = true
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
     }
-  
+
     this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
