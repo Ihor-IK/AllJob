98bea1d7aa30ce31e900837df53ddddd1752c405
Ihor Kukharev <kuharev@ukr.net>
2020-01-05 19:18:37 -0500

    Added output jobs

--- alljob/src/app/home-page/home-page.component.ts
+++ alljob/src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {JobsService} from '../shared/jobs.service';
+import {Observable} from 'rxjs';
+import {Job} from '../shared/interfaces';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  jobs$: Observable<Job[]>
+
+  constructor(private jobsService: JobsService) {
+  }
 
   ngOnInit() {
+    this.jobs$ = this.jobsService.getAll()
   }
 
 }
